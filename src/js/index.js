8db4ce705a4f85a0c355581959131b9cc52c5a28
Amol Borgaonkar <amol.borgaonkar@ad.infosys.com>
2019-11-28 16:35:13 +0530

    Finish Webpack Babel

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// Global app controller
+
+import num from './test';
+const x = 24;
+console.log('num', num);
+console.log('x', x);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'), // __dirname - current absolute path
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist' // serve this folder
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
